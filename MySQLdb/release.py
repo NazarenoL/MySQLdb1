f901e53f6ede0e9617b92883f9175eb87e80b206
Nazareno Lorenzo <nazareno.lorenzo@reddit.com>
2019-12-19 14:08:59 -0800

    Fake bump more

--- MySQLdb/release.py
+++ MySQLdb/release.py
@@ -1,4 +1,4 @@
 
 __author__ = "Andy Dustman <farcepest@gmail.com>"
-version_info = (1,2,'6bionic','final',1)
-__version__ = "1.2.6bionic"
+version_info = (1,2,'5bionic','final',1)
+__version__ = "1.2.5bionic"
